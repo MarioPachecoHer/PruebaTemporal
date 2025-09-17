c2f41bc6c51aa74646c8bbc41b3eb8834455fe9a
Your Name <you@example.com>
2025-09-17 16:05:57 -0600

    Imprime en pantalla los repositorios encontrados

--- githubscanner.py
+++ githubscanner.py
@@ -10,3 +10,11 @@ git_dir = r"C:\Users\mario\OneDrive\Desktop\4 Cuatri\Programacion de redes\pract
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+if repos:
+    print("Found the following Github repositories.")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx}. {repo}") 
+    else:
+        print("No GitHb repositories found.")
+        print("Please provide a GitHub repository")
